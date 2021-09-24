978e0781fbd7a333365ab42c6256460b5238ab15
Fernando Poncio <fecrackle@gmail.com>
2021-09-23 22:37:23 -0600

    uso del modulo Global

---
+++ modulos/globales.js
@@ -0,0 +1,16 @@
+// let i = 0;
+
+// let intervalo = setInterval(() => {
+//     console.log("Hola")
+//     if (i === 3) {
+//         clearInterval(intervalo)
+//     }
+//     i++;
+// }, 1000);
+
+// setImmediate(() => {
+//     console.log("Hola");
+// });
+
+global.miVariable = "El valor";
+console.log(miVariable);
