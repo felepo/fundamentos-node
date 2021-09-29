a70ba073db066a3c9350b0ed1d1beac58a700da4
Fernando Poncio <fecrackle@gmail.com>
2021-09-28 22:11:59 -0600

    uso de console

---
+++ modulos/consola.js
@@ -0,0 +1,45 @@
+// console.group('Conversacion');
+// console.log('Hola');
+// console.group('Bla');
+// console.log('Blablabla');
+// console.log('Blablabla');
+// console.log('Blablabla');
+// console.groupEnd('Bla');
+// console.log('Adios');
+// console.groupEnd('Conversacion');
+
+// console.log('Otra cosa de por ahi');
+
+// -----------------------------------------------------------------------
+
+// function funcion1() {
+//     console.group('funcion 1');
+//     console.log('Esto es de la funcion 1');
+//     console.log('Esto tambien');
+//     console.log('Esto tambien');
+//     funcion2();
+//     console.log('Hemos vuelto a la funcion 1');
+//     console.groupEnd('funcion 1');
+// }
+
+// function funcion2() {
+//     console.group('funcion 2');
+//     console.log('Estamos en la funcion 2');
+//     console.log('Esto tambien en la funcion 2');
+//     console.groupEnd('funcion 2');
+// }
+
+// console.log('Empezamos');
+// funcion1();
+
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
+console.countReset('veces');
+console.count('veces');
+console.count('veces');
+console.count('veces');
