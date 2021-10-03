0415cffc013be3dec957248deb673043c397bde8
Fernando Poncio <fecrackle@gmail.com>
2021-10-03 00:01:12 -0600

    manejo de Error First Callbacks

---
+++ trucos/errorFirst.js
@@ -0,0 +1,22 @@
+function asincrona(callback) {
+    setTimeout(() => {
+        try {
+            let a = 3 + z;
+            callback(null, a);
+        } catch (error) {
+            callback(error);
+        }
+    }, 1000);
+}
+
+asincrona((err, dato) => {
+    if(err) {
+        console.error('tenemos un error:');
+        console.error(err);
+
+        return false;
+    }
+
+    console.log('todo ha ido bien, mi data es:');
+    console.log(dato);
+})
